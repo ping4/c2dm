00aa6cd6a5318334d482a6361675b3e4296b019f
Keenan Brock <keenan@thebrocks.net>
2013-05-16 13:09:41 -0400

    parse responses

--- lib/c2dm.rb
+++ lib/c2dm.rb
@@ -1,7 +1,9 @@
 require 'httparty'
 require 'cgi'
+require 'uri'
 
 class C2DM
+  class InvalidAuth < StandardError ; end
   include HTTParty
   default_timeout 30
 
@@ -13,35 +15,32 @@ class C2DM
   def initialize()
   end
 
-  def authenticate!(username, password, source = nil)
+  def authenticate!(username, password, source)
     auth_options = {
       'accountType' => 'HOSTED_OR_GOOGLE',
       'service'     => 'ac2dm',
       'Email'       => username,
       'Passwd'      => password,
-      'source'      => source || 'MyCompany-MyAppName-1.0'
+      'source'      => source
     }
     response = post_message(AUTH_URL,auth_options, nil)
+    body = Hash[response.body.split("\n").map {|r| r.split("=")}]
 
     # check for authentication failures
-    if response['Error=']
-      raise response.parsed_response
+    if body['Error']
+      raise raise InvalidAuth.new(body['Error'])
     else
-      @auth_token = response.body.split("\n")[2].gsub('Auth=', '')
+      @auth_token = body['Auth']
     end
 
     self
   end
 
   def send_notifications(notifications = [])
-    notifications.collect do |notification|
-      {
-        :body => c2dm.send_notification(notification),
-        :registration_id => notification[:registration_id]
-      }
-    end
+    notifications.collect { |notification| c2dm.send_notification(notification) }
   end
 
+  # input:
   # {
   #   :registration_id => "...",
   #   :data => {
@@ -50,10 +49,54 @@ class C2DM
   #   }
   #   :collapse_key => "optional collapse_key string"
   # }
+  # results:
+  # {
+  #   registration_id: "...",
+  #   body: response
+  # }
   def send_notification(options)
     options[:collapse_key] ||= 'foo'
     response = post_message(PUSH_URL, options)
+    body = Hash[URI.decode_www_form(response.body)]
 
+    result = {
+      code:            response.code,
+      response:        body['Error'],
+      body:            response,
+      registration_id: options[:registration_id]
+    }
+
+
+#    body = Hash[response.body.split(('&').map{|l| l.split('=')})]
+    headers = response.headers
+
+    case response.code
+    when 200
+      case body['Error']
+      when 'QuotaExceeded', 'DeviceQuotaExceeded' #406 Not Acceptable
+        result[:code]=406
+      when 'DeviceQuotaExceeded' #406 Not Acceptable
+        result[:code]=406
+      when 'InvalidRegistration', 'NotRegistered' #404 Not Found
+        result[:code]=404
+      when 'MessageTooBig', 'MissingCollapseKey' #400 Bad Request
+        result[:code]=400
+        #invalid message
+      else
+        result[:response]='Success' #200 Created
+      end
+    when 401
+      result[:response]='AUTH_TOKEN' #401 Unauthorized
+      raise InvalidAuth.new(response.body)
+    when 404
+      # concerned that a 404 will get confused with token not found
+      raise InvalidAuth.new(response.body)
+    when 503
+      result[:response]='Retry' #503 Server Unavailable
+      result[:retry_after]=response.headers['Retry-After']
+    end
+
+    result
   end
 
   private
@@ -78,8 +121,8 @@ class C2DM
     post_body = build_post_body(params)
 
     post_params = {
-      :body    => post_body,
-      :headers => {
+      body:    post_body,
+      headers: {
         'Content-type'   => 'application/x-www-form-urlencoded',
         'Content-length' => post_body.length.to_s
       }
