b885fe0c8d6f4ffa2290b27a2b0faeabcba45d5b
Keenan Brock <keenan@thebrocks.net>
2013-05-16 10:32:58 -0400

    convert all static methods into instance methods

--- lib/c2dm.rb
+++ lib/c2dm.rb
@@ -10,65 +10,44 @@ class C2DM
   AUTH_URL = 'https://www.google.com/accounts/ClientLogin'
   PUSH_URL = 'https://android.apis.google.com/c2dm/send'
 
-  class << self
-    attr_accessor :auth_token
-
-    def authenticate!(username, password, source = nil)
-      auth_options = {
-        'accountType' => 'HOSTED_OR_GOOGLE',
-        'service'     => 'ac2dm',
-        'Email'       => username,
-        'Passwd'      => password,
-        'source'      => source || 'MyCompany-MyAppName-1.0'
-      }
-      post_body = build_post_body(auth_options)
-
-      params = {
-        :body    => post_body,
-        :headers => {
-          'Content-type'   => 'application/x-www-form-urlencoded',
-          'Content-length' => post_body.length.to_s
-        }
-      }
-
-      response = self.post(AUTH_URL, params)
-
-      # check for authentication failures
-      raise response.parsed_response if response['Error=']
-
-      @auth_token = response.body.split("\n")[2].gsub('Auth=', '')
-    end
-
-    def send_notifications(notifications = [])
-      c2dm = C2DM.new(@auth_token)
-      notifications.collect do |notification|
-        {
-          :body => c2dm.send_notification(notification),
-          :registration_id => notification[:registration_id]
-        }
-      end
-    end
-    
-    def build_post_body(options={})
-      post_body = []
-
-      # data attributes need a key in the form of "data.key"...
-      data_attributes = options.delete(:data)
-      data_attributes.each_pair do |k,v|
-        post_body << "data.#{k}=#{CGI::escape(v.to_s)}"
-      end if data_attributes
-
-      options.each_pair do |k,v|
-        post_body << "#{k}=#{CGI::escape(v.to_s)}"
-      end
-
-      post_body.join('&')
-    end
-    
+  def initialize()
   end
 
-  def initialize(auth_token = nil)
-    @auth_token = auth_token || self.class.auth_token
+  def authenticate!(username, password, source = nil)
+    auth_options = {
+      'accountType' => 'HOSTED_OR_GOOGLE',
+      'service'     => 'ac2dm',
+      'Email'       => username,
+      'Passwd'      => password,
+      'source'      => source || 'MyCompany-MyAppName-1.0'
+    }
+    post_body = build_post_body(auth_options)
+
+    params = {
+      :body    => post_body,
+      :headers => {
+        'Content-type'   => 'application/x-www-form-urlencoded',
+        'Content-length' => post_body.length.to_s
+      }
+    }
+
+    response = http_post(AUTH_URL, params)
+
+    # check for authentication failures
+    raise response.parsed_response if response['Error=']
+
+    @auth_token = response.body.split("\n")[2].gsub('Auth=', '')
+
+    self
+  end
+
+  def send_notifications(notifications = [])
+    notifications.collect do |notification|
+      {
+        :body => c2dm.send_notification(notification),
+        :registration_id => notification[:registration_id]
+      }
+    end
   end
 
   # {
@@ -81,7 +60,7 @@ class C2DM
   # }
   def send_notification(options)
     options[:collapse_key] ||= 'foo'
-    post_body = self.class.build_post_body(options)
+    post_body = build_post_body(options)
 
     params = {
       :body    => post_body,
@@ -92,7 +71,28 @@ class C2DM
       }
     }
 
-    self.class.post(PUSH_URL, params)
+    http_post(PUSH_URL, params)
   end
 
+  private
+
+  def build_post_body(options={})
+    post_body = []
+
+    # data attributes need a key in the form of "data.key"...
+    data_attributes = options.delete(:data)
+    data_attributes.each_pair do |k,v|
+      post_body << "data.#{k}=#{CGI::escape(v.to_s)}"
+    end if data_attributes
+
+    options.each_pair do |k,v|
+      post_body << "#{k}=#{CGI::escape(v.to_s)}"
+    end
+
+    post_body.join('&')
+  end
+
+  def http_post(url, params)
+    response = self.class.post(url, params)
+  end
 end
